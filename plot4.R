18998b1d6b4f4699f0c1a3f7b9ed9ed8cb814b7f
tex <jmpd1988@gmail.com>
2018-11-28 14:50:05 -0500

    submission for the coursera project

--- plot4.R
+++ plot4.R
@@ -18,7 +18,7 @@ with(data,
      x = ymd_hms(paste(Date, Time, sep= ' ')),
      y = Global_active_power,
      type = "l",
-     ylab = "Global Active Power (Kilowatts)",
+     ylab = "Global Active Power",
      xlab = ""
    )
 )
